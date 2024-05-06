7f1f493021d2f93df0fbb32001c54050cadc1f6a
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-05-06 12:11:22 -0400

    Add Inquirer version 8.2.4 for user input handling. Implement logic to generate SVG file for Triangle shape based on user input.

--- index.js
+++ index.js
@@ -6,26 +6,41 @@ const Circle = require('./lib/circle')
 
 const questions = [
     {
-        type:'list',
-        message:'Choose the following shape:',
-        name:'shape',
-        choices:['circle', 'square', 'triangle']
+        type: 'list',
+        message: 'Choose the following shape:',
+        name: 'shape',
+        choices: ['circle', 'square', 'triangle']
     },
     {
-        type:'input',
-        message:'Enter three characters:',
-        name:'text',
+        type: 'input',
+        message: 'Enter three characters:',
+        name: 'text',
     },
     {
-        type:'input',
-        message:'Enter text color:',
-        name:'textColor',
+        type: 'input',
+        message: 'Enter text color:',
+        name: 'textColor',
     },
     {
-        type:'input',
-        message:'Enter shape color:',
-        name:'shapeColor',
+        type: 'input',
+        message: 'Enter shape color:',
+        name: 'shapeColor',
     }
 ]
+inquirer.prompt(questions)
+    .then(data => {
+        if (data.shape === 'circle') {
+
+        }
+        else if (data.shape === 'triangle') {
+            const triangle = new Triangle(data.text, data.textColor, data.shapeColor)
+            fs.writeFile('./examples/triangle.svg', triangle.render(), err=>{
+                if(err) console.error(err)
+                console.log('success')
+            })
+        } else {
+
+        }
+    })
 
 
