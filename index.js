bd6b17b8686d54e3ec7c5e9e6a107856c570b0d6
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-05-06 08:25:21 -0400

    Define questions array for shape selection and input gathering

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 
+const questions = [
+    {
+        type:'list',
+        message:'Choose the following shape:',
+        name:'shape',
+        choices:['circle', 'square', 'triangle']
+    },
+    {
+        type:'input',
+        message:'Enter three characters:',
+        name:'text',
+    },
+    {
+        type:'input',
+        message:'Enter text color:',
+        name:'textColor',
+    },
+    {
+        type:'input',
+        message:'Enter shape color:',
+        name:'shapeColor',
+    }
+]
 
 inquirer.prompt(questions)
