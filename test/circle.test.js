1489e4d5406a9346f28f8da80698a7361636a838
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-05-05 23:23:40 -0400

    Implement Circle class render method test in circle.test.js to validate SVG generation with expected text, text color, and shape color.

--- test/circle.test.js
+++ test/circle.test.js
@@ -1,8 +1,8 @@
 const Circle = require('../lib/circle')
+const circle = new Circle('PLL','blue','black')
 describe('Circle', ()=>{
         describe('properties', ()=>{
             it('test text, textColor and shapeColor',()=>{
-                const circle = new Circle('PLL','blue','black')
                 expect(circle.text).toEqual('PLL')
                 expect(circle.textColor).toEqual('blue')
                 expect(circle.shapeColor).toEqual('black')
@@ -12,7 +12,7 @@ describe('Circle', ()=>{
 
         describe('method (is function in an object', ()=>{
               it('test render()',()=>{
-
+                expect(circle.render()).toEqual(`<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200"><circle cx="150" cy="100" r="80" fill=black/><text x="150" y="125" font-size="60" text-anchor="middle" fill=blue>PLL</text></svg>` )
               })
         })
 })
